db43e98b1f3e6c0727c734aaf7c19fb066358fed
Doug DeJong <dougdejong@gmail.com>
2018-10-26 18:19:45 -0400

    fixing access tokens

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -27,39 +27,70 @@ router.get('/oauth/redirect', (req, res) => {
   const requestToken = req.query.code
 
 
-  var options = {
-    "method": "POST",
-    "hostname": "api.dexcom.com",
-    "port": null,
-    "path": "/v2/oauth2/token",
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded",
-      "cache-control": "no-cache"
-    }
-  };
-  
-  var req = http.request(options, function (res) {
-    var chunks = [];
-  
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-  
-    res.on("end", function () {
-      var body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
-  });
 
-  console.log(clientID, clientSecret, requestToken, clientURI)
-  
-  req.write(qs.stringify({ client_secret: clientSecret,
-    client_id: clientID,
-    code: requestToken,
-    grant_type: 'authorization_code',
-    redirect_uri: clientURI }));
-  req.end();
 
+  axios({
+    method: 'post',
+    url: `https://sandbox-api.dexcom.com/v2/oauth2/token?client_secret=${clientSecret}&client_id=${clientID}&code=${requestToken}&grant_type=authorization_code&redirect_uri=${clientURI}`,
+    headers: {
+         accept: 'application/x-www-form-urlencoded'
+    },
+    
+  }).then((response) => {
+
+    console.log('$$$$$$$$$$$$$$$');
+    console.log(response.data)
+
+    const accessToken = response.data.access_token
+    // redirect the user to the welcome page, along with the access token
+    res.redirect(`/index.html?access_token=${accessToken}`)
+  })
+  .catch((err) => {
+    console.log('@#$@#$!#@$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$')
+    console.log(req.query)
+    console.log(err)
+  })
+
+
+
+
+
+
+  // var options = {
+  //   "method": "POST",
+  //   "hostname": "sandbox-api.dexcom.com",
+  //   "port": null,
+  //   "path": "/v2/oauth2/token",
+  //   "headers": {
+  //     "content-type": "application/x-www-form-urlencoded",
+  //     "cache-control": "no-cache"
+  //   }
+  // };
+  
+  // var req = http.request(options, function (res) {
+  //   var chunks = [];
+  
+  //   res.on("data", function (chunk) {
+  //     chunks.push(chunk);
+  //   });
+  
+  //   res.on("end", function () {
+  //     var body = Buffer.concat(chunks);
+  //     console.log(body.toString());
+  //   });
+  // });
+
+  
+  // req.write(qs.stringify({ client_secret: `${clientSecret}`,
+  //   client_id: `${clientID}`,
+  //   code: `${requestToken}`,
+  //   grant_type: 'authorization_code',
+  //   redirect_uri: `${clientURI}`,
+  //   scope: 'offline_access'}));
+  // req.end();
+
+
+  // res.redirect('/', {credentials: res})
 
 });
 
