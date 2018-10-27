0d150e6537983e45edad45dcae3d7a0d7e9257cd
Doug DeJong <dougdejong@gmail.com>
2018-10-26 20:32:19 -0400

    pre postman testing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,69 +19,69 @@ const clientURI = `http://localhost:${process.env.PORT}/oauth/redirect`
 
 
 // Declare the redirect route
-router.get('/oauth/redirect', (req, res) => {
+// router.get('/oauth/redirect', (req, res) => {
 
 
-  if (res) {
-    console.log('############')
-    console.log(res)
+//   if (res) {
+//     console.log('############')
+//     console.log(res)
 
-    res.render('success', {auth: res.data})
-  }
-  const requestToken = req.query.code
-
+//     res.render('success', {auth: res.data})
+//   }
 
 
 
-  // axios({
+
+// axios({
   //   method: 'post',
   //   url: `https://sandbox-api.dexcom.com/v2/oauth2/token?client_secret=${clientSecret}&client_id=${clientID}&code=${requestToken}&grant_type=authorization_code&redirect_uri=${clientURI}&scope=offline_access`,
   //   headers: {
-  //        accept: 'application/x-www-form-urlencoded'
-  //   },
+    //        accept: 'application/x-www-form-urlencoded'
+    //   },
     
-  // }).then((response) => {
-
-  //   console.log('$$$$$$$$$$$$$$$');
-  //   console.log(response.data)
-
-  //   const accessToken = response.data.access_token
-  //   // redirect the user to the welcome page, along with the access token
-  //   res.redirect(`/index.html?access_token=${accessToken}`)
-  // })
-  // .catch((err) => {
-  //   console.log('@#$@#$!#@$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$')
-  //   console.log(req.query)
-  //   console.log(err)
-  // })
-
-
-
-
-
-
-  var options = {
-    "method": "POST",
-    "hostname": "sandbox-api.dexcom.com",
-    "port": null,
-    "path": "/v2/oauth2/token",
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded",
-      "cache-control": "no-cache"
-    }
-  };
-  
-  var req = http.request(options, function (res) {
-    var chunks = [];
-  
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-  
-    res.on("end", function () {
-      var body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
+    // }).then((response) => {
+      
+      //   console.log('$$$$$$$$$$$$$$$');
+      //   console.log(response.data)
+      
+      //   const accessToken = response.data.access_token
+      //   // redirect the user to the welcome page, along with the access token
+      //   res.redirect(`/index.html?access_token=${accessToken}`)
+      // })
+      // .catch((err) => {
+        //   console.log('@#$@#$!#@$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$!@#$')
+        //   console.log(req.query)
+        //   console.log(err)
+        // })
+        
+        
+        
+        
+        
+        
+        var options = {
+          "method": "POST",
+          "hostname": "sandbox-api.dexcom.com",
+          "port": null,
+          "path": "/v2/oauth2/token",
+          "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "cache-control": "no-cache"
+          }
+        };
+        const requestToken = req.query.code
+        
+        var req = http.request(options, function (res) {
+          var chunks = [];
+          
+          res.on("data", function (chunk) {
+            chunks.push(chunk);
+          });
+          
+          res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+          });
   });
 
   
@@ -93,9 +93,7 @@ router.get('/oauth/redirect', (req, res) => {
     scope: 'offline_access'}))
     req.end()
 
-  
-  })
-  
+
   ;
   
 
